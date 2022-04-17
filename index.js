4be803b40b27ffb1620362d6a940dec8b528f88b
josep andreu <josepa45@gmail.com>
2022-04-17 13:58:04 +0200

    configurado acceso a los hermanos y bordes definidos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const rows = 8;
-const columns = 5;
+const rows = 5;
+const columns = 8;
 
 class Ficha {
   ejeX;
@@ -32,26 +32,78 @@ const dead = [];
 const estadoFicha = () => {
   for (let i = 0; i < grid.length; i++) {
     if (grid[i].live === true) {
-      alive.push(grid[i]);
+      alive.push(`${grid[i].id} ${grid[i].live}`);
     } else {
-      dead.push(grid[i]);
+      dead.push(`${grid[i].id} ${grid[i].live}`);
     }
   }
   return alive;
 };
 
-const neightbours = () => {
+const mappingBeightbours = () => {
   for (let i = 0; i < grid.length; i++) {
-    const id = i + 1;
-    grid[i].id = id;
-    grid[i].rightId = grid[id + 1];
-    grid[i].leftId = grid[id - 1];
-    grid[i].topId = grid[id + columns];
-    grid[i].bottomId = grid[id - columns];
-    grid[i].rightTopId = grid[id + columns + 1];
-    grid[i].leftTopId = grid[id + columns - 1];
-    grid[i].rightBottomId = grid[id - columns + 1];
-    grid[i].leftBottomId = grid[id - columns - 1];
+    const idValue = i + 1;
+
+    grid[i].id = idValue;
+    if (typeof grid[i - columns] === "undefined") {
+      grid[i].topId = "border";
+    } else {
+      grid[i].topId = grid[i - columns].live;
+    }
+
+    if (
+      typeof grid[i + 1 - columns] === "undefined" ||
+      (i + 1) % columns === 0
+    ) {
+      grid[i].rightTopCornerId = "border";
+    } else {
+      grid[i].rightTopCornerId = grid[i + 1 - columns].live;
+    }
+
+    if (typeof grid[i + 1] === "undefined" || (i + 1) % columns === 0) {
+      grid[i].rightId = "border";
+    } else {
+      grid[i].rightId = grid[i + 1].live;
+    }
+
+    if (
+      typeof grid[i + 1 + columns] === "undefined" ||
+      (i + 1) % columns === 0
+    ) {
+      grid[i].rightBottomCornerID = "border";
+    } else {
+      grid[i].rightBottomCornerID = grid[i + 1 + columns].live;
+    }
+
+    if (typeof grid[i + columns] === "undefined") {
+      grid[i].bottomId = "border";
+    } else {
+      grid[i].bottomId = grid[i + columns].live;
+    }
+
+    if (
+      typeof grid[i - 1 + columns] === "undefined" ||
+      (i + columns) % columns === 0
+    ) {
+      grid[i].leftBottomCornerID = "border";
+    } else {
+      grid[i].leftBottomCornerID = grid[i - 1 + columns].live;
+    }
+
+    if (typeof grid[i - 1] === "undefined" || (i + columns) % columns === 0) {
+      grid[i].leftId = "border";
+    } else {
+      grid[i].leftId = grid[i - 1].live;
+    }
+
+    if (
+      typeof grid[i - 1 - columns] === "undefined" ||
+      (i + columns) % columns === 0
+    ) {
+      grid[i].letfTopCornerId = "border";
+    } else {
+      grid[i].letfTopCornerId = grid[i - 1 - columns].live;
+    }
   }
 };
 
@@ -63,5 +115,5 @@ grid[21 - 1].live = true;
 grid[22 - 1].live = true;
 grid[28 - 1].live = true;
 
-neightbours();
+mappingBeightbours();
 estadoFicha();
